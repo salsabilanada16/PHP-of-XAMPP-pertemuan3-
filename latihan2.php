11ea5fed073773856e4c87e61edc496f934381fd
salsabilanada16 <sadzani16@gmail.com>
2020-02-21 15:07:54 +0700

    latihan2 php pertemuan3

---
+++ latihan2.php
@@ -0,0 +1,8 @@
+<?php
+    // Pengulangan
+    // for                                      // kalau for itu semua menyatu jadi lebih praktis
+
+    for($i = 1; $i <= 10; $i++) {
+        echo "Hello World $i kali! <br>";
+    }
+?>
