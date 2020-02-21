dda3a03d1eb5abcfece408ed682b0d409d6a8bf5
salsabilanada16 <sadzani16@gmail.com>
2020-02-21 14:24:32 +0700

    file php pertemuan3

---
+++ angkot1.php
@@ -0,0 +1,8 @@
+<?php
+
+$no_angkot = 1;                                            
+while($no_angkot <= 10) {                                   
+    echo "Angkot No. $no_angkot beroperasi dengan baik. <br>";
+    $no_angkot++;                                           
+}
+?>
